acf5aa7430cd6172ab3eead1d899fab090d596dc
unknown <luckynhkim@naver.com>
2025-08-11 18:27:18 +0900

    2025-08-11

---
+++ python/Baekjoon/2018_투 포인터_1.py
@@ -0,0 +1,19 @@
+N = int(input()) 
+count = 1 
+start_index = 1 
+end_index = 1 
+sum = 1
+
+while end_index != N:
+    if sum == N:
+        count += 1
+        end_index += 1 
+        sum += end_index 
+    elif sum > N:
+        sum -= start_index 
+        start_index += 1 
+    else:
+        end_index += 1 
+        sum += end_index 
+
+print(count)
